da66807c8bf20cac90a5c39373c1a4780879d4c2
Nguyễn Đặng Hoài Nam <stghoainam4002@gmail.com>
2025-10-19 01:02:48 +0700

    Config migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,15 @@
 from logging.config import fileConfig
-
+import sys
+import os
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # để có thể import từ project root
+from config.env import settings
+from database.database import database
+from model import base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,11 +20,13 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option("sqlalchemy.url", database.DATABASE_URL)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = base.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- model/base.py
+++ model/base.py
@@ -1,13 +1,18 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, UUID, DateTime, Boolean
+from datetime import datetime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, DateTime, Boolean
+from uuid import uuid4
+import uuid
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 class BaseEntity(Base):
-    __abstract__ = True 
-    id = Column(UUID, primary_key=True)
-    requestedAt = Column(DateTime)
-    requestedIp = Column(String(100))
-    isDeleted = Column(Boolean, default=False)
-    updatedAt = Column(DateTime)
-    updatedBy = Column(String(100))
+    __abstract__ = True
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid4)
+    requestedAt: Mapped[datetime | None] = mapped_column(DateTime, default=datetime.utcnow)
+    requestedIp: Mapped[str | None] = mapped_column(String(100))
+    isDeleted: Mapped[bool] = mapped_column(Boolean, default=False)
+    updatedAt: Mapped[datetime | None] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    updatedBy: Mapped[str | None] = mapped_column(String(100))
